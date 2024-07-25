387f98e12e19064262c356350c163dce76f2ebf6
Ahmad Ibrahim <168755972+Searcher06@users.noreply.github.com>
2024-07-25 12:46:27 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,69 @@
+/*
+var arrayOfQuote = [];
+arrayOfQuote[0] = ["Ahmad Ibrahim"]
+arrayOfQuote[1] = "If you want to achieve greatness stop asking for permission. ~Anonymous";
+arrayOfQuote[2] = "The greatest successes comes from having the freedom to fail ~Mark Zuckerberge";
+arrayOfQuote[3] = "If you are not willing to risk the usual you will have to settle for the ordinary. ~Jim Rohn";
+arrayOfQuote[4] = "Take up one idea. Make that one idea your life - think of it, dream of it, live on that idea. Let the brain, muscles, nerves, every part of your body, be full of that idea, and just leave every other idea alone. This is the way to success. ~Swami Vivekananda";
+arrayOfQuote[5] = "All our dreams can come true if we have the courage to pursue them. ~Walt Disney";
+arrayOfQuote[6] = "Good things come to people who wait, but better things come to those who go out and get them. ~ Anonymous";
+arrayOfQuote[7] = "Success is walking from failure to failure with no loss of enthusiasm. ~ Winstom Churchill";
+arrayOfQuote[8] = "Opportunities don't happen, you create them. ~ Chris Grosser";
+arrayOfQuote[9] = "Try not to become a person of success, but rather try to become a person of value. ~ Albert Einstein";
+arrayOfQuote[10] = "Great minds discuss ideas; average minds discuss events; small minds discuss people. ~ Eleanor Roosevelt";
+arrayOfQuote[11] = "I have not faild. I'just found 10,000 ways that won't work. ~ Thomas A. Edison";
+function generate() {
+  let randomQoute = arrayOfQuote[Math.floor(Math.random() * 12)];
+  return quote.innerHTML = randomQoute;
+}
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*var quoteArray = [
+  {
+    text: "The greatest successes comes from having the freedom to fail",
+    author: "Mark Zuckerberge",
+  },
+  {
+    text: "Life is the most difficult exam,people used to copy others not realizing that everyone has a different question paper",
+    author: "Ahmad Ibrahim",
+  },
+  { text: "Quote 3", author: "Unknown" },
+  { text: "Quote 3", author: "Unknown" },
+  { text: "Quote 3", author: "Unknown" },
+  { text: "Quote 3", author: "Unknown" },
+  { text: "Quote 3", author: "Unknown" },
+  { text: "Quote 3", author: "Unknown" },
+  { text: "Quote 3", author: "Unknown" },
+  { text: "Quote 3", author: "Unknown" },
+  { text: "Quote 3", author: "Unknown" },
+];
+
+function generate() {}*/
